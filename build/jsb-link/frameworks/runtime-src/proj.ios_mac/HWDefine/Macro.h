1476e01d2de46ed796db49ad32d67e82ad71ac3b
杜宾 <>
2023-03-30 15:13:46 +0800

    fix

---
+++ build/jsb-link/frameworks/runtime-src/proj.ios_mac/HWDefine/Macro.h
@@ -0,0 +1,15 @@
+//
+//  Macro.h
+//  hdwinged
+//
+//  Created by 杜宾 on 2023/3/30.
+//
+
+#ifndef Macro_h
+#define Macro_h
+
+#define HW_topOnAd_APPID @"a64242a6c20eca"
+#define HW_topOnAd_APPkey @"48512db29762ce894edfa9e279614e30"
+
+
+#endif /* Macro_h */
---
+++ build/jsb-link/frameworks/runtime-src/proj.ios_mac/Pods/AnyThinkiOS/AnyThinkiOS/AnyThinkFacebookAdapter.framework/Headers/AnyThinkFacebookAdapter.h
@@ -0,0 +1,20 @@
+//
+//  AnyThinkFacebookAdapter.h
+//  AnyThinkFacebookAdapter
+//
+//  Created by Topon on 11/14/20.
+//  Copyright © 2020 AnyThink. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <AnyThinkFacebookAdapter/ATFacebookConfigure.h>
+
+//! Project version number for AnyThinkFacebookAdapter.
+FOUNDATION_EXPORT double AnyThinkFacebookAdapterVersionNumber;
+
+//! Project version string for AnyThinkFacebookAdapter.
+FOUNDATION_EXPORT const unsigned char AnyThinkFacebookAdapterVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AnyThinkFacebookAdapter/PublicHeader.h>
+
+
